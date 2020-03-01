a6acac3a9026d22f1ee658dfbb414ba47d7c0505
Ave Afif <avafif@gmail.com>
2020-03-01 10:21:37 +0700

    Add ESLint

--- pages/browse.js
+++ pages/browse.js
@@ -1,7 +1,7 @@
 export default function Browse() {
-    return (
-      <div>
-        <p>Browse</p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <p>Browse</p>
+    </div>
+  );
+}
--- pages/user.js
+++ pages/user.js
@@ -1,7 +1,7 @@
 export default function User() {
-    return (
-      <div>
-        <p>User Detail</p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <p>User Detail</p>
+    </div>
+  );
+}
